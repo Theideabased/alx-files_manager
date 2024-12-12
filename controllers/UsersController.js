c3ee3e02a386840e3a93188a72a5ae94a8eae0b8
Theideabased <ogunmusireseyi01@gmail.com>
2024-12-12 10:40:51 +0100

    adding a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -18,5 +18,54 @@ class UsersController {
     email: same as the value received
     password: SHA1 value of the value received
      */
-    
+    static async postNew(request, response) {
+        const { email, password } = request.body;
+        
+        if (!email) return response.status(400).send({error: 'Missing email'});
+        if (!password) { return response.status(400).send({ error: 'Missing password'});}
+
+        const emailExists = await dbClient.usersCollection.findOne( {email });
+
+        if (emailExists) { return response.status(400).send({ error: 'Already exist '});}
+
+        const sha1Passwrord = sha1(password);
+
+        let result;
+
+        try {
+            result = await dbClient.usersCollection.insertOne({
+                email,
+                password: sha1Passwrord,
+            });
+        }catch (err) {
+            await userQueue.add({});
+            return response.status(500).send({error: 'Error creating user '});
+        }
+
+        const user = {
+            id: result.InsertedId,
+            email,
+        };
+        await userQueue.add({
+            userId: result.insertedId.toString(),
+        });
+        return response.status(201).send(user);
+        }
+
+        static async getMe(request, response) {
+            const { userId } = await userUtils.getUserIdAndKey(request);
+
+            const user = await userUtils.getUser({
+                _id: ObjectId(userId),
+            });
+            if (!user) return response.status(401).send({error: 'Unauthorized'});
+
+            const processedUser = {id: user._id, ...user};
+            delete processedUser._id;
+            delete processedUser.password;
+
+            return response.status(200).send(processedUser);
+        }
 }
+
+export default UsersController;
